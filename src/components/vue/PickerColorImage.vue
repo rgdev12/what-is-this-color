aede9cd1f3b1ce94c4b5fc989d35778b2af490a0
Román González <roman.montano12@gmail.com>
2024-05-03 18:35:06 -0600

    🔨 adding drag and drop functionality

--- src/components/vue/PickerColorImage.vue
+++ src/components/vue/PickerColorImage.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="bg-white rounded-md py-10 px-2 sm:px-0 flex justify-center items-center">
     <Toast v-if="showToast" :message="toastMessage" @close="closeToast"/>
-    <div class="max-w-96 sm:max-w-[550px] w-full border-2 p-3 rounded-md">
-      <div class="relative w-full h-32 rounded-sm flex justify-center items-center flex-col border-2 border-dashed cursor-pointer" @click="openFile">
-        <input @change="loadImage" type="file" id="upload-image" accept="image/*" hidden>
-        <img class="" src="src/assets/icons/Upload.svg" alt="Upload Icon" />
-        <h3>Cargar imagen</h3>
-        <p class="text-sm sm:text-base">arrastra y suelta o selecciona una imagen</p>
+    <div class="max-w-96 sm:max-w-[570px] w-full border-2 p-3 rounded-md">
+      <div class="rounded-sm border-2 border-dashed cursor-pointer"
+           @dragover.prevent="onDragOver" @dragleave.prevent="onDragLeave" @drop.prevent="drop" @click="openFile">
+        <div v-if="!isDragging" class="relative w-full h-32 flex justify-center items-center flex-col">
+          <input @change="loadImage" type="file" id="upload-image" accept="image/*" hidden>
+          <img class="" src="src/assets/icons/Upload.svg" alt="Upload Icon" />
+          <h3>Cargar imagen</h3>
+          <p class="text-sm sm:text-base">arrastra y suelta o selecciona una imagen</p>
+        </div>
+
+        <div v-else class="relative w-full h-32 flex justify-center items-center flex-col">
+          <p>Soltar aquí</p>
+        </div>
       </div>
 
       <div class="mt-5 w-full">
-        <img src="" alt="" id="show-image">
+        <img @click="test" src="" id="show-image">
       </div>
     </div>
   </div>
@@ -20,8 +27,21 @@
 import { ref } from 'vue';
 import Toast from './Toast.vue';
 
+declare global {
+  class EyeDropper {
+    open(): Promise<{ sRGBHex: string }>;
+    close(): void;
+  }
+}
+
 let toastMessage = ref('');
 let showToast = ref(false);
+
+let isDragging = ref(false);
+
+function test() {
+  console.log('test');
+}
 
 function openFile() {
   const input = document.getElementById('upload-image');
@@ -30,22 +50,56 @@ function openFile() {
 
 function loadImage() {
   const input = document.getElementById('upload-image') as HTMLInputElement;
-  const image = document.getElementById('show-image') as HTMLImageElement;
 
   if (input.files && input.files[0]) {
     // validamos que sea una imagen
     if (!input.files[0].type.includes('image')) {
       toastMessage.value = 'El archivo seleccionado no es una imagen';
       showToast.value = true;
+
+      // limpiamos el input
+      input.value = '';
       return;
     }
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      image.src = e.target?.result as string;
-    };
-    reader.readAsDataURL(input.files[0]);
+    showImage(input.files[0]);
   }
+}
+
+function onDragOver(event: any) {
+  event.preventDefault();
+  isDragging.value = true;
+}
+
+function onDragLeave(event: any) {
+  event.preventDefault();
+  isDragging.value = false;
+}
+
+function drop(event: any) {
+  event.preventDefault();
+  isDragging.value = false;
+  const file = event.dataTransfer.files[0];
+
+  // validamos que sea una imagen
+  if (!file.type.includes('image')) {
+    toastMessage.value = 'El archivo seleccionado no es una imagen';
+    showToast.value = true;
+    return;
+  }
+
+  showImage(file);
+
+  event.dataTransfer.clearData();
+}
+
+function showImage(file: File) {
+  const image = document.getElementById('show-image') as HTMLImageElement;
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    image.src = e.target?.result as string;
+  };
+  reader.readAsDataURL(file);
 }
 
 function closeToast() {
